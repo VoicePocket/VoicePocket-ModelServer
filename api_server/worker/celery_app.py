65bdf381bde178af7b5c1f2161b340a6548b5ef8
hi-june <wnsgur735@gmail.com>
2023-02-02 21:29:28 +0900

    chore: 도커 환경 세팅

--- api_server/worker/celery_app.py
+++ api_server/worker/celery_app.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 wav_worker = Celery(
     'wav_worker',
-    broker='',# '{transport}://{userid}:{password}@{hostname}:{port}/{virtual_host}'
+    broker='amqp://pocket:pocket!@rabbit/voice_pocket_host',# '{transport}://{userid}:{password}@{hostname}:{port}/{virtual_host}'
     backend='rpc://',
     include=['api_server.worker.wav_tasks'],
 )
