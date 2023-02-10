0820fa1323dfd1d46a1b1b766edc9e3d74142644
hi-june <wnsgur735@gmail.com>
2023-02-10 21:49:25 +0900

    feat: gcs bucket에 파일 업로드 및 다운로드 구현

--- api_server/tts_process.py
+++ api_server/tts_process.py
@@ -1,8 +1,9 @@
-import os, sys
+import os, sys, tarfile
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from module.TTS.TTS.utils.synthesizer import Synthesizer
 from text_process import normalize_text
+from bucket_process import down_model_from_bucket, upload_wav_to_bucket
 
 def is_set(email):
     if synth_dict.get(email) == None:
@@ -10,9 +11,15 @@ def is_set(email):
     return True
 
 def add_synth(email):
-    voice_model_path = f"./api_server/voice_model/{email}/"
-    # TODO: voice_model_path에 파일이 존재하지 않을 시 버킷에서 다운로드 받는 코드
+    # TODO: best 모델 구성이 결정되면 그에 맞춰 코드를 수정할 것!
+    # voice_model_path = f"./api_server/voice_model/{email}_best_model.pth.tar"
     
+    # if not os.path.isfile(voice_model_path):
+    #     down_model_from_bucket(email)
+        
+    #     best_model_tar = tarfile.open(voice_model_path)
+    #     best_model_tar.extractall(f"./api_server/voice_model")
+
     synthesizer = Synthesizer(
         "./api_server/voice_model/glowtts/5g_checkpoint.pth.tar",
         "./api_server/voice_model/glowtts/5g_config.json",
@@ -23,20 +30,21 @@ def add_synth(email):
         None,
         False,
     )
-    
-    # TODO: synthesizer 설정이 끝나면 다운 받은 모델파일을 제거(서버 메모리 문제)
 
     synth_dict[email] = synthesizer
 
+    # os.remove(voice_model_path)
+
 def make_tts(email, uuid, text):
-    wav_path = f'./api_server/wav/{email}_{uuid}.wav'
+    wav_path = f'./api_server/wav/{uuid}.wav'
     syn = synth_dict.get(email)
     symbol = syn.tts_config.characters.characters
     text = normalize_text(text, symbol)
     wav = syn.tts(text, None, None)
     syn.save_wav(wav, wav_path)
-    # TODO: 만든 wav파일을 bucket에 업로드해야 함
-    # TODO: 업로드 후 wav파일 제거
-
+    
+    upload_wav_to_bucket(wav_path, email, uuid)
+    
+    os.remove(wav_path)
 
 synth_dict = {} # syntesizer 객체를 모아두는 딕셔너리
